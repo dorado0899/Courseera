c94a60ad0db702b382c0470be127e97892eede5e
dorado0899 <66026360+dorado0899@users.noreply.github.com>
2020-06-08 09:16:44 -0400

    Update script.js

--- docs/Week04/script.js
+++ docs/Week04/script.js
@@ -1,22 +1,22 @@
 /*
-
+Solution of assignment 4:
 
 Expected output:
 
-Hello Yusuf
-Good Bye Jimmy
-Good Bye Jerry
-Good Bye Jacob
-Hello Pushti	
-Hello Darsh
-Hello Minal
-Hello Rekha
-Hello Aarya
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
 Good Bye Jim
 */
 
 (function() {
-    var names = ["Yusuf", "Jimmy", "Jerry", "Jacob", "Pushti", "Darsh", "Minal", "Rekha", "Aarya", "Jim"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
